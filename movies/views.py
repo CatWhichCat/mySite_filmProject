3a2c6e1f9fa0ac16400db9759150a70da7805410
CatWhichCat <fortunalich@mail.ru>
2022-07-19 14:51:30 +0600

    html, css (templayes, static) - надо подключить css файл к своему шаблону

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
+from .models import *
 # Create your views here.
+
+class MovieView(View):
+    '''Список фильмов'''
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movie_list.html", {'movie_list': movies})
